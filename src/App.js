e307de33032e588390cc9a816634215a7025c435
Bharath <bharathraj7302@gmail.com>
2023-02-21 21:24:06 +0530

    google auth

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
 import { CreateContainer, MainContainer } from "./components";
+import { AnimatePresence } from "framer-motion";
 
 const App = () => {
   return (
-    <div className="w-screen h-auto flex flex-col bg-red-50">
-      <Header />
-      <main className="mt-24 p-8 w-full ">
-        <Routes>
-          <Route path="/" element={<MainContainer />} />
-          <Route path="/createItem" element={<CreateContainer />} />
-        </Routes>
-      </main>
-    </div>
+    <AnimatePresence>
+      <div className="w-screen h-auto flex flex-col bg-zinc-100">
+        <Header />
+        <main className="mt-24 p-8 w-full ">
+          <Routes>
+            <Route path="/" element={<MainContainer />} />
+            <Route path="/createItem" element={<CreateContainer />} />
+          </Routes>
+        </main>
+      </div>
+    </AnimatePresence>
   );
 };
 export default App;
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const MainContainer = () => {
-  return <div></div>;
+  return <div>
+    Main
+  </div>;
 };
 
 export default MainContainer;
