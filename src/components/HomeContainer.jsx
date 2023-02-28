2aabbf441bc346bb88aa10681191d5f2015555dc
Bharath <bharathraj7302@gmail.com>
2023-02-28 21:07:02 +0530

    header

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Delivery from "../img/delivery.png";
 import HeroBg from "../img/heroBg.png";
+import { heroData } from "../utils/Data";
 
 const HomeContainer = () => {
   return (
@@ -42,15 +43,35 @@ const HomeContainer = () => {
         </button>
       </div>
 
-      <div className="py-2 flex-1 flex items-center">
+      <div className="py-2 flex-1 flex items-center relative">
         <img
           src={HeroBg}
           className="ml-auto h-420 w-full lg:w-auto lg:h-650"
           alt="hero-bg"
         />
-      </div>
 
-      <div className="w-full h-full absolute flex items-center justify-center"></div>
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-20 py-4 gap-4 flex-wrap">
+          {heroData &&
+            heroData.map((n) => (
+              <div
+                key={n.id}
+                className=" lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
+              >
+                <img className=" w-20 lg:w-40 -mt-10 lg:-mt-20" src={n.imageSrc} alt="I1" />
+                <p className=" text-base lg:text-xl font-semibold text-textColor mt-2  lg:mt-4">
+                  {n.name}
+                </p>
+                <p className=" text-[12px] lg:text-sm text-lighttextGray font-semibold my-3">
+                  {n.desc}
+                </p>
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600 ">₹</span>
+                  {n.price}
+                </p>
+              </div>
+            ))}
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/utils/Data.js
@@ -0,0 +1,36 @@
+import I1 from "../img/i1.png";
+import F1 from "../img/f1.png";
+import C3 from "../img/c3.png";
+import Fi1 from "../img/fi1.png";
+
+export const heroData = [
+  {
+    id: 1,
+    name: "IceCream",
+    desc: "Chocolate & Vanilla",
+    price: "₹ 120",
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "Strawberries",
+    desc: "Fresh Strawberries",
+    price: "₹ 80",
+    imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: "Chicken Kebab",
+    desc: "Mixed Kebab Plate",
+    price: "₹150",
+    imageSrc: C3,
+  },
+  {
+    id: 4,
+    name: "Fish Kebab",
+    desc: "Mixed Fish Kebab",
+    price: "₹240",
+    imageSrc: Fi1,
+  },
+
+];
