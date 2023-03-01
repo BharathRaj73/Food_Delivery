1ab089817544625eb2fcfa69b3e946de74c3a464
Bharath <bharathraj7302@gmail.com>
2023-03-01 20:04:48 +0530

    create new item

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -59,16 +59,28 @@ const Header = () => {
             exit={{ opacity: 0, x: 200 }}
             className="flex items-center gap-8 "
           >
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+              onClick={() => setIsMenu(false)}
+            >
               Menu
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+              onClick={() => setIsMenu(false)}
+            >
               About Us
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+              onClick={() => setIsMenu(false)}
+            >
               Home
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+              onClick={() => setIsMenu(false)}
+            >
               Service
             </li>
           </motion.ul>
@@ -97,7 +109,10 @@ const Header = () => {
               >
                 {user && user.email === "bharathraj7302@gmail.com" && (
                   <Link to={"./createItem"}>
-                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                    <p
+                      className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                      onClick={() => setIsMenu(false)}
+                    >
                       New Item <MdAdd />
                     </p>
                   </Link>
--- src/utils/Data.js
+++ src/utils/Data.js
@@ -8,29 +8,66 @@ export const heroData = [
     id: 1,
     name: "IceCream",
     desc: "Chocolate & Vanilla",
-    price: "₹ 120",
+    price: "120",
     imageSrc: I1,
   },
   {
     id: 2,
     name: "Strawberries",
     desc: "Fresh Strawberries",
-    price: "₹ 80",
+    price: "80",
     imageSrc: F1,
   },
   {
     id: 3,
     name: "Chicken Kebab",
     desc: "Mixed Kebab Plate",
-    price: "₹150",
+    price: "150",
     imageSrc: C3,
   },
   {
     id: 4,
     name: "Fish Kebab",
     desc: "Mixed Fish Kebab",
-    price: "₹240",
+    price: "240",
     imageSrc: Fi1,
   },
+];
 
+export const categories = [
+  {
+    id: 1,
+    name: "Chicken",
+    urlParamName: "chicken",
+  },
+  {
+    id: 2,
+    name: "Curry",
+    urlParamName: "curry",
+  },
+  {
+    id: 3,
+    name: "Rice",
+    urlParamName: "rice",
+  },
+  {
+    id: 4,
+    name: "Fish",
+    urlParamName: "fish",
+  },
+  {
+    id: 5,
+    name: "Fruits",
+    urlParamName: "fruits",
+  },
+  {
+    id: 6,
+    name: "Icecreams",
+    urlParamName: "icecreams",
+  },
+  {
+    id: 7,
+    name: "Soft Drinks",
+    urlParamName: "drinks",
+  },
 ];
